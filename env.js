9e030122ce9778597da5e2dbf2f82f42a658e46a
shakeel157 <workshakeel15@gmail.com>
2025-08-21 16:24:12 +0100

    add treatment api

--- env.js
+++ env.js
@@ -14,7 +14,9 @@ const config = {
     stripeCheckout: process.env.STRIPE_CHECKOUT_URL || "https://stripe.faithdiscipline.org.uk/stripe-checkout.php",
     getAllPayments: process.env.GET_ALL_PAYMENTS_URL || "https://stripe.faithdiscipline.org.uk/Payments_APIs/get_all_payments.php",
     editPayment: process.env.EDIT_PAYMENT_URL || "https://stripe.faithdiscipline.org.uk/Payments_APIs/edit_payments.php",
-    adminLogin: process.env.ADMIN_LOGIN_URL || "https://stripe.faithdiscipline.org.uk/login.php"
+    adminLogin: process.env.ADMIN_LOGIN_URL || "https://stripe.faithdiscipline.org.uk/login.php",
+
+    medication: process.env.MEDICATION_URL || "https://medication-service.onrender.com"
   }
 };
 
